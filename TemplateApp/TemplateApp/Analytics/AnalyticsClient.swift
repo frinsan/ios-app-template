310a65c8ee3b47e92b8660d0afcdb8c640dd47e4
Genesan Kim <genesan@gmail.com>
2025-11-30 15:16:21 -0800

    Avoid AnalyticsEvent name clash with legacy client

--- TemplateApp/TemplateApp/Analytics/AnalyticsClient.swift
+++ TemplateApp/TemplateApp/Analytics/AnalyticsClient.swift
@@ -10,7 +10,7 @@ enum AnalyticsEventName: String {
     case accountDeleted
 }
 
-struct AnalyticsEvent {
+private struct LegacyAnalyticsEvent {
     let name: AnalyticsEventName
     let properties: [String: String]?
     let timestamp: Date
@@ -23,7 +23,7 @@ final class AnalyticsClient {
     private init() {}
 
     func track(_ name: AnalyticsEventName, properties: [String: String]? = nil) {
-        let event = AnalyticsEvent(name: name, properties: properties, timestamp: Date())
+        let event = LegacyAnalyticsEvent(name: name, properties: properties, timestamp: Date())
         queue.async {
             #if DEBUG
             print("[Analytics]", event.name.rawValue, event.properties ?? [:])
